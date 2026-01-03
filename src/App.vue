4826695d4f0db0a620bc1046c3b260ce408831df
Charles Bridgers IV <charlesbridgersiv@gmail.com>
2026-01-03 15:35:07 -0600

    Initial commit: Stranger Things Melody app setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="app-container">
+        <div class="outlined header">Welcome to HawkinS</div>
+    </div>
+</template>
+
+<style scoped></style>
